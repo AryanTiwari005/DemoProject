9b7c844698d8e6608fb89a6ed68b25b3c56a0fb2
Aryan Tiwari <tiwariaryan.2005@gmail.com>
2026-02-06 16:31:06 +0530

    normal practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
 import './App.css'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+// import Registration from './components/Registration';
 
 export default function App() {
+      const arr= [1,2,3,4,5];
+    const c = {
+        name: "KIET",
+        location: "Ghaziabad",
+        established: 1998,
+        students: 10000
+    }
+    
   
-
   return (
-   <h1>Hello Vite + React!<span><br></br> hii</span></h1>
-
-  )
+    
+    <>
+     <Header title="My First React App"/>
+     <Header title="Welcome User"/>
+     <Footer year={arr} company={c} />
+    </>
+  );
 }
 
-// export default App
+
+
